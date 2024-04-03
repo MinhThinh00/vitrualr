4fec453d79c1f9d91b843e54bee2aa166520bb35
Minh Thịnh <111744097+MinhThinh00@users.noreply.github.com>
2024-04-03 14:41:32 +0700

    first commit

--- src/index.js
+++ src/index.js
@@ -2,16 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+
+import Banner from './components/Banner';
+import Navbar from './components/Navbar';
+import Feature from './components/Feature';
+import Workflow from './components/Workflow';
+import Pricing from './components/Pricing';
+import Testimonials from './components/Testimonials';
+import Footer from './components/Footer';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <Navbar/>
+    <div className='max-w-7xl mx-auto pt-20 px-6'>
+      <Banner />
+      <Feature/>
+      <Workflow/>
+      <Pricing/>
+      <Testimonials/>
+      <Footer/>
+    </div>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
